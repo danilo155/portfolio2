72215478df061edf2dc454d005471c13ec9a34ca
данило <playgroop@ukr.net>
2022-06-11 15:26:23 +0300

    main

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <li class="logo"></li>
+    <div class="links">
+      <Router-link to="/home">Home</Router-link>
+      <Router-link to="/about">About</Router-link>
+      <Router-link to="/Tech-Stack">Tech Stack</Router-link>
+      <Router-link to="/Projects">Projects</Router-link>
+      <Router-link to="/Contact">Contact</Router-link>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+nav{
+  display: flex;
+  justify-content: space-between;
+}
+li{
+  list-style: none;
+}
+a{
+  text-decoration: none;
+  margin-left: 67px;
+  color: #000;
+}
+a:first-child{
+  margin-left: 0;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import { createApp, Vue } from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
-
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 createApp(App).use(router).mount('#app')
